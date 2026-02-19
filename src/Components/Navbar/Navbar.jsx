10bb2feeb18dfe9266e0badceeafbe0ee3fb884e
nancy nyokabi <nancynyokabi1995@gmail.com>
2026-02-19 13:55:11 +0300

    Added the Home /About pages

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,38 +1,22 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
-function Navbar() {
+function Navbar({ toggle, open }) { 
   return (
-    <nav className={styles.nav}>
-      <h2 className={styles.logo}>ShareAMeal</h2>
-
-      <div className={styles.links}>
-        <NavLink
-          to="/sponsor"
-          className={({ isActive }) =>
-            isActive ? `${styles.link} ${styles.active}` : styles.link
-          }
-        >
-          Sponsor
-        </NavLink>
-
-        <NavLink
-          to="/sme"
-          className={({ isActive }) =>
-            isActive ? `${styles.link} ${styles.active}` : styles.link
-          }
-        >
-          SME
-        </NavLink>
-
-        <NavLink
-          to="/ngo"
-          className={({ isActive }) =>
-            isActive ? `${styles.link} ${styles.active}` : styles.link
-          }
-        >
-          NGO
-        </NavLink>
+    <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <Link to="/"><img src="/logo.png" alt="Logo" className={styles.logoImg} /></Link>
+      </div>
+      <button className={styles.menuBtn} onClick={toggle}>
+        {open ? "✕" : "☰"}
+      </button>
+      <div className={`${styles.links} ${open ? styles.active : ""}`}>
+        <div className={styles.mobileLogoContainer}>
+           <img src="/logo.png" alt="Logo" className={styles.logoImg} />
+        </div>
+        <Link to="/" onClick={toggle}>Home</Link>
+        <Link to="/about" onClick={toggle}>About</Link>
+        <Link to="/services" onClick={toggle}>Services</Link>
       </div>
     </nav>
   );
